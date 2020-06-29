7d230a2f3406b20f16bbf79544483532fd6c0fff
STnetwork <admin@stnetwork.fr>
2020-06-29 13:57:07 +0200

    Update gatsby-config.js
    add theme cactus theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,16 @@ module.exports = {
         path: `${__dirname}/src/pages`,
         name: 'pages',
       },
+    {
+      resolve: `@chrismwilliams/gatsby-theme-cactus`,
+      options: {
+        /*
+          This theme passes all the options to its base theme, gatsby-theme-blog-core
+          - basePath: defaults to '/'
+          - contentPath: defaults to 'content/posts'
+          - assetPath: default to 'content/assets'
+        */
+      },
     },
     {
       resolve: 'gatsby-source-cosmicjs',
